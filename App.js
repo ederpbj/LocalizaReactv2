97bac03008a08953ec54b6a5a35e2bb633486dc9
ederpbj <ederpbj@yahoo.com.br>
2019-09-30 14:13:49 -0300

    Botões de localização, muda cidade

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { Text, Button, StyleSheet, View } from 'react-native'
 import MapView, {Marker} from 'react-native-maps'
 
 export default class App extends Component {
@@ -9,12 +9,25 @@ export default class App extends Component {
       lat:-7.2379005,
       lng: -35.8858189
     }
+
+    this.moverMapa = this.moverMapa.bind(this);
+    
+    /*
+    SP: -7.2379005, -35.8858189
+    BH: -19.9027026, -44.0340895
+    DF: -15.8080374, -47.8750231
+  */
+
   } 
-  /*
-         SP: -7.2379005, -35.8858189
-         BH: -19.9027026, -44.0340895
-         DF: -15.8080374, -47.8750231
-     */
+
+
+  moverMapa(lat, lng){
+    let state = this.state;
+    state.lat = lat;
+    state.lng = lng;
+    this.setState(state);
+  }  
+
   render() {
    return (
     <View style={styles.container}>
@@ -28,15 +41,15 @@ export default class App extends Component {
       longitudeDelta: 0.0121,
       }}
      >
-       <Marker coordinate={{latitude:37.78825, longitude:-122.4324 }} title="São Paulo" />
+       <Marker coordinate={{latitude:this.state.lat, longitude:this.state.lng }} title="São Paulo" />
      </MapView>
 
      <Text style={styles.texto}>{this.state.lat} X {this.state.lng}</Text>
 
      <View style={styles.buttonArea}>
-       <Button title="São Paulo" onPress={} />
-       <Button title="Belo Horizonte" onPress={} />
-       <Button title="Brasília" onPress={} />
+       <Button style={styles.botton} title="São Paulo" onPress={()=>{this.moverMapa(-7.2379005, -35.8858189)}} />
+       <Button style={styles.botton} title="Belo Horizonte" onPress={()=>{this.moverMapa(-19.9027026, -44.0340895)}} />
+       <Button style={styles.botton} title="Brasília" onPress={()=>{this.moverMapa(-15.8080374, -47.8750231)}} />
      </View>
     </View>
     );
@@ -59,9 +72,15 @@ const styles = StyleSheet.create({
     bottom: 0
    }, */
    texto:{
-     fontSize:25
+     fontSize:25,
+     marginTop:20,
+     marginBottom:20,
+     textAlign:'center'
    },
    buttonArea:{
      flexDirection:'row'
+   },
+   botton:{
+     flex:1
    }
 })
