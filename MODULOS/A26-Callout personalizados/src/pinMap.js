d4496703028bd83e9dcb87de8d04d55ca485a57f
ederpbj <ederpbj@yahoo.com.br>
2019-10-05 16:37:32 -0300

    A26-Callout personalizados

---
+++ MODULOS/A26-Callout personalizados/src/pinMap.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+export default class PinMapa extends Component {
+  render() {
+    return (
+      <View
+        style={(pinMapStyles.quadrado, {backgroundColor: this.props.fundo, width:80})}>
+        <Text style={pinMapStyles.texto}>{this.props.contaMedia}</Text>
+      </View>
+    );
+  }
+}
+
+const pinMapStyles = StyleSheet.create({
+  quadrado: {
+    backgroundColor: '#FF0000',
+    width: 50,
+    height: 50,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  texto: {
+    color: '#FFFFFF',
+    fontSize: 20,
+  },
+});
